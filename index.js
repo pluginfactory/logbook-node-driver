e4bcb921d4988face190c308c82a5e9bf41e7cd8
gaurav <sharma02gaurav@gmail.com>
2019-03-06 23:45:38 +0530

    initial commit by scaffold with basic configuration

---
+++ index.js
@@ -0,0 +1,12 @@
+
+/**
+ * This will export all the required packages.
+ * Acts as an indexer
+ *
+ * @author gaurav sharma
+ * @since Wednesday, December 19, 2018
+ */
+import Service from './src/service';
+export default {
+	PluginService: Service,
+};
